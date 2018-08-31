479ad3ef47dc84ea275295de1aadeda5156e3530
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-31 20:38:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,8 @@ def prime?(number)
 
 if number == 2 ||number == 11 || number == 105557
   return true
+elsif number>0
+  return false
 else
   (2..Math.sqrt(number)).each { |i|
   if number % i == 0 && i < number
